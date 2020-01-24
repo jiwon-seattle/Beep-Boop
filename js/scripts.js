810a870847f067bea638b564f8fdce5d05c5989c
Jiwon Han <jiwon1.han@gmail.com>
2020-01-24 15:10:05 -0800

    Complete another for loops that shows list of numbers

--- js/scripts.js
+++ js/scripts.js
@@ -10,41 +10,35 @@ $(document).ready(function() {
     var boop = "Boop!"
     var sorry = "I'm sorry, Dave. I'm afraid I can't do that."
 
-    for (x=0; x <= number; x++) {
-      integers.push(x)
-      console.log(numbers)
-    }
-
-
-    for (y=0; y < integers.length; y++) {
       if (num === "13") {
         result.push("I'm sorry, Dave. I'm afraid I can't do that.")
-        console.push(result)
+
       } else if (num === "21") {
         result.push(boop)
       } else if (num === "32") {
-        result.text("I'm sorry, Dave. I'm afraid I can't do that.")
+        result.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (num === "0") {
+        result.push('"0"')
       } else {
         for (i = 0; i < num.length; i++) {
           if (num.charAt(i) === "1") {
             result.push(beep)
-            console.log(result)
           } else if (num.charAt(i) === "2") {
             result.push(boop)
           } else if (num.charAt(i) === "3") {
             result.push(sorry)
+          } else {
+            result.push(num)
           }
         }
       }
+
+    for (x=0; x <= number; x++) {
+      integers.push(x)
     }
 
-
-
-
-
-
-    $("#result").text(result);
-    $("#result").text(numbers)
+    $("#result1").append(result + ",");
+    $("#result2").text(integers)
   });
 
 });
