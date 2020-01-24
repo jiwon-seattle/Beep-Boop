aa97a7af870075bafa9ee68ea5db8edc676c17ad
Jiwon Han <jiwon1.han@gmail.com>
2020-01-24 10:04:47 -0800

    Attempt making structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,11 @@
-
 $(document).ready(function() {
-  $("button#start").click(function(event){
-    $("form#language").show();
-  })
-})
-
-$(document).ready(function() {
-  $("form#language").submit(function(event) {
+  $("#input-form").submit(function(event){
     event.preventDefault();
-    var data = $("input:radio[name=data]:checked").val();
-    var duration = $("input:radio[name=duration]:checked").val();
-    var result;
-    if (duration === "long") {
-      $("#result-long").show();
-      $("#result-intro").show();
-      $("#result-sql").show();
-      $("#result-python").show();
-      $("#result-javascript").show();
-    } else if (duration === "short" && data === "sql2") {
-      $("#result-intro").show();
-      $("#result-sql").show();
-      $("#result-python").hide();
-      $("#result-javascript").hide();
-    } else if (duration === "short" && data === "python2") {
-      $("#result-intro").show();
-      $("#result-sql").hide();
-      $("#result-python").show();
-      $("result-javascript").hide();
-    } else if (duration === "short" && data === "javascript2") {
-      $("#result-intro").show();
-      $("#result-sql").hide();
-      $("#result-python").hide();
-      $("#result-javascript").show();
-    } else if (duration === "short" && duration === "long") {
-      $("#result-intro").show();
-    }
+    var num = $("#user-input").val();
 
-    var nameInput = $("input#name").val();
-    $("#you").text(nameInput)
 
-    $("#output").show();
-    $("#ramen").text(result);
-    $("#recipient").text(nameInput);
-    $("#location").text(addressInput);
-    $("#phone").text(contactInput);
+    $("#result").text(num);
 
   });
+
 });
