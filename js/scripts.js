2cdbf0de0ca666c22865a76cc935f1dd7820ddfc
Jiwon Han <jiwon1.han@gmail.com>
2020-01-24 11:03:31 -0800

    Add charAt function for if statement that checking elements

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
     event.preventDefault();
     var num = $("#user-input").val();
 
+    for (i = 0; i < num.length; i++) {
+      if (num.charAt(i) === 1) {
+        console.log("hi")
+      } else {
+        console.log('hello')
+      }
+    }
+
+
 
     $("#result").text(num);
 
