b69582cce31011bc91f8721232e1d4376fb31a22
Jiwon Han <jiwon1.han@gmail.com>
2020-01-25 17:11:45 -0800

    Complete Beep Boop

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,13 @@ $(document).ready(function() {
     var boop = "Boop!"
     var sorry = "I'm sorry, Dave. I'm afraid I can't do that."
 
+    var one = /[1]/i;
+    var two = /[2]/i;
+    var three = /[3]/i;
+
+
       if (num === "13") {
         result.push("I'm sorry, Dave. I'm afraid I can't do that.")
-
       } else if (num === "21") {
         result.push(boop)
       } else if (num === "32") {
@@ -21,14 +25,19 @@ $(document).ready(function() {
         result.push('"0"')
       } else {
         for (i = 0; i < num.length; i++) {
-          if (num.charAt(i) === "1") {
+          if (num[i].match(one)) {
             result.push(beep)
-          } else if (num.charAt(i) === "2") {
+            console.log(result)
+            break
+          } else if (num[i].match(two)) {
             result.push(boop)
-          } else if (num.charAt(i) === "3") {
+            break
+          } else if (num[i].match(three)) {
             result.push(sorry)
+            break
           } else {
             result.push(num)
+            break
           }
         }
       }
